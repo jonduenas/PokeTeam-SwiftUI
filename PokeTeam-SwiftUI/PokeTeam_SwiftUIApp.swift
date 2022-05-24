7dc7de1ee5ed8a40319da4f189fa9d21c632cefd
Jon Duenas <jon.duenas@willowtreeapps.com>
2022-05-24 12:33:45 -0400

    Initial Commit

---
+++ PokeTeam-SwiftUI/PokeTeam_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeTeam_SwiftUIApp.swift
+//  PokeTeam-SwiftUI
+//
+//  Created by Jon Duenas on 5/24/22.
+//
+
+import SwiftUI
+
+@main
+struct PokeTeam_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
